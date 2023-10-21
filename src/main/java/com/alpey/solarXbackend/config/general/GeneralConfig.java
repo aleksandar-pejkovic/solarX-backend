fba44132ab0b86bb0b47ff4034b00f42e4733cfe
aleksandar-pejkovic <aleksandar.pejkovic89@gmail.com>
2023-10-22 01:13:41 +0200

    add clients and company

--- src/main/java/com/alpey/solarXbackend/config/general/GeneralConfig.java
+++ src/main/java/com/alpey/solarXbackend/config/general/GeneralConfig.java
@@ -5,9 +5,6 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.templatemode.TemplateMode;
-import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 @Configuration
 @EnableAsync
@@ -19,23 +16,5 @@ public class GeneralConfig {
         ModelMapper modelMapper = new ModelMapper();
         modelMapper.getConfiguration().setSkipNullEnabled(true);
         return modelMapper;
-    }
-
-    @Bean
-    public TemplateEngine templateEngine() {
-        TemplateEngine templateEngine = new TemplateEngine();
-        templateEngine.setTemplateResolver(webPageTemplateResolver());
-        return templateEngine;
-    }
-
-    @Bean
-    public ClassLoaderTemplateResolver webPageTemplateResolver() {
-        ClassLoaderTemplateResolver webPageTemplateResolver = new ClassLoaderTemplateResolver();
-        webPageTemplateResolver.setPrefix("templates/");
-        webPageTemplateResolver.setSuffix(".html");
-        webPageTemplateResolver.setTemplateMode(TemplateMode.HTML);
-        webPageTemplateResolver.setCharacterEncoding("UTF-8");
-        webPageTemplateResolver.setOrder(1);
-        return webPageTemplateResolver;
     }
 }
---
+++ src/main/java/com/alpey/solarXbackend/model/entity/Client.java
@@ -0,0 +1,52 @@
+package com.alpey.solarXbackend.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "clients")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    private String director;
+
+    @Column(nullable = false, unique = true)
+    private String registrationNumber;
+
+    @Column(nullable = false, unique = true)
+    private String taxNumber;
+
+    @Column(nullable = false, unique = true)
+    private String bankAccount;
+
+    private String street;
+
+    private String zip;
+
+    private String city;
+
+    private String phone;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    private String website;
+}
