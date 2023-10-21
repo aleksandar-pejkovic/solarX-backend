db0ed8dd89cbde40e6d2846de93b7888dc4ddb56
aleksandar-pejkovic <aleksandar.pejkovic89@gmail.com>
2023-10-21 18:09:54 +0200

    first commit

---
+++ src/main/java/com/alpey/solarXbackend/SolarXBackendApplication.java
@@ -0,0 +1,13 @@
+package com.alpey.solarXbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolarXBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolarXBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alpey/solarXbackend/SolarXBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alpey.solarXbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolarXBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
